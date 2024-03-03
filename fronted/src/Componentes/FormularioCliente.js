e525f512502d98516f119c78d4cb9d6f4de22ef0
Bryan Adrian Iza Oña <65301459+Baio99@users.noreply.github.com>
2024-03-02 23:55:13 -0500

    Arrreglo estados

--- fronted/src/Componentes/FormularioCliente.js
+++ fronted/src/Componentes/FormularioCliente.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom'; // Importar el componente Link
 
 const FormularioCliente = ({ idHabitacion }) => {
   const [cedula, setCedula] = useState('');
@@ -201,6 +202,7 @@ const FormularioCliente = ({ idHabitacion }) => {
        
 
           <button type="submit">Confirmar Reserva</button>
+          <Link to="/Hoteles">Cancelar</Link> {/* Botón de Cancelar con redirección */}
         </form>
       )}
     </div>
