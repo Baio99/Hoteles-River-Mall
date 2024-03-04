ff66617af6239f6916a9a43ec8bb619fc36fc00e
AndyPilozo <88100571+adpilozo@users.noreply.github.com>
2024-03-03 22:41:29 -0500

    Update FormularioCliente.js

--- fronted/src/Componentes/FormularioCliente.js
+++ fronted/src/Componentes/FormularioCliente.js
@@ -13,32 +13,33 @@ const FormularioCliente = ({ idHabitacion }) => {
   const [reservaConfirmada, setReservaConfirmada] = useState(false);
   const [fechaIngreso, setFechaIngreso] = useState('');
   const [fechaSalida, setFechaSalida] = useState('');
-  const [estatusRes, setEstatusRes] = useState('Pend'); // Valor predeterminado 'Pend'
+  const [estatusRes, setEstatusRes] = useState('Pendiente'); // Valor predeterminado 'Pend'
   //const [mostrarEstadoReservacion, setMostrarEstadoReservacion] = useState(false); // Estado para controlar la visibilidad del campo Estado Reservación
   const [showModal, setShowModal] = useState(false); // Estado para controlar si se muestra el modal
   const navigate = useNavigate(); // Hook useNavigate para la navegación
 
-// Componente de modal para mostrar los datos del usuario
-const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabitacion, onClose }) => {
-  return (
-    <div className="modal-background">
-      <div className="modal-content">
-        <h2>Datos Usuario</h2>
-        <p>Cédula: {cedula}</p>
-        <p>Nombre: {nombre}</p>
-        <p>Fecha de Ingreso: {fechaIngreso}</p>
-        <p>Fecha de Salida: {fechaSalida}</p>
-        <p>Tipo de Habitación: {tipoHabitacion}</p>
-      
+  // Componente de modal para mostrar los datos del usuario
+  const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabitacion, onClose }) => {
+    return (
+
+      <div className="modal-background" >
+        <div className="modal-content">
+          <h2>Datos Usuario</h2>
+          <p>Cédula: {cedula}</p>
+          <p>Nombre: {nombre}</p>
+          <p>Fecha de Ingreso: {fechaIngreso}</p>
+          <p>Fecha de Salida: {fechaSalida}</p>
+          <p>Tipo de Habitación: {tipoHabitacion}</p>
+
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
 
   const handleClienteSubmit = async (e) => {
     e.preventDefault();
     // Cambiar el estatus_res a 'Conf' al confirmar la reserva
-    
+
     const clienteData = {
       cedula_cliente: cedula,
       apellidos_cliente: apellido,
@@ -70,12 +71,12 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
 
   const handleReservacionSubmit = async (e) => {
     e.preventDefault();
-    setEstatusRes('Conf');
-    
+    setEstatusRes('Confirmada');
 
-     // Ajustar el formato de las fechas al formato esperado en la base de datos (DD-MM-YYYY)
-     const fechaIngresoFormatted = formatDate(fechaIngreso);
-     const fechaSalidaFormatted = formatDate(fechaSalida);
+
+    // Ajustar el formato de las fechas al formato esperado en la base de datos (DD-MM-YYYY)
+    const fechaIngresoFormatted = formatDate(fechaIngreso);
+    const fechaSalidaFormatted = formatDate(fechaSalida);
 
     const reservacionData = {
       cedula_cliente: cedula,
@@ -106,8 +107,8 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
     }
   };
 
-   // Función para ajustar el formato de la fecha
-   const formatDate = (dateString) => {
+  // Función para ajustar el formato de la fecha
+  const formatDate = (dateString) => {
     const [year, month, day] = dateString.split('-');
     return `${day}-${month}-${year}`;
 
@@ -122,12 +123,12 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
     // Cerrar el modal cambiando el estado
     setShowModal(false);
   };
-  
+
 
   return (
-    <div>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       {!reservaConfirmada ? (
-        <form onSubmit={handleClienteSubmit}>
+        <form onSubmit={handleClienteSubmit} style={{  width: '300px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
           <h2>Registrar Cliente</h2>
           {/* Campos del formulario para registrar cliente */}
           <div>
@@ -138,6 +139,7 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
               value={cedula}
               onChange={(e) => setCedula(e.target.value)}
               required
+              style={{ marginLeft: '30px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
             />
           </div>
           <div>
@@ -148,6 +150,7 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
               value={apellido}
               onChange={(e) => setApellido(e.target.value)}
               required
+              style={{ marginLeft: '22px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
             />
           </div>
           <div>
@@ -158,6 +161,7 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
               value={nombre}
               onChange={(e) => setNombre(e.target.value)}
               required
+              style={{ marginLeft: '22px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
             />
           </div>
           <div>
@@ -168,6 +172,7 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
               value={direccion}
               onChange={(e) => setDireccion(e.target.value)}
               required
+              style={{ marginLeft: '13px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
             />
           </div>
           <div>
@@ -178,6 +183,7 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
               value={telefono}
               onChange={(e) => setTelefono(e.target.value)}
               required
+              style={{ marginLeft: '20px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
             />
           </div>
           <div>
@@ -188,33 +194,39 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
+              style={{ marginLeft: '39px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
             />
           </div>
-          <button type="submit">Registrar Cliente</button>
+
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <button type="submit" style={{ backgroundColor: 'green', color: 'white', padding: '7px', border: 'none', borderRadius: '5px' }}>Registrar Cliente</button>
+          </div>
         </form>
       ) : (
-        <form onSubmit={handleReservacionSubmit}>
+        <form onSubmit={handleReservacionSubmit} style={{  width: '350px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
           <h2>Confirmar Reserva</h2>
           {/* Campos del formulario para confirmar reservación */}
           <div>
-          <label htmlFor="fechaIngreso">Fecha de ingreso:</label>
-        <input
-          type="date"
-          id="fechaIngreso"
-          value={fechaIngreso}
-          onChange={(e) => setFechaIngreso(e.target.value)}
-          required
-        />
+            <label htmlFor="fechaIngreso">Fecha de ingreso:</label>
+            <input
+              type="date"
+              id="fechaIngreso"
+              value={fechaIngreso}
+              onChange={(e) => setFechaIngreso(e.target.value)}
+              required
+              style={{ marginLeft: '35px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
+            />
           </div>
           <div>
-          <label htmlFor="fechaSalida">Fecha de salida:</label>
-        <input
-          type="date"
-          id="fechaSalida"
-          value={fechaSalida}
-          onChange={(e) => setFechaSalida(e.target.value)}
-          required
-        />
+            <label htmlFor="fechaSalida">Fecha de salida:</label>
+            <input
+              type="date"
+              id="fechaSalida"
+              value={fechaSalida}
+              onChange={(e) => setFechaSalida(e.target.value)}
+              required
+              style={{ marginLeft: '45px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px' }}
+            />
           </div>
 
           <div>
@@ -224,16 +236,19 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
               id="estadoReservacion"
               value={estatusRes}
               readOnly // Hacer que el campo sea de solo lectura
+              required
+              style={{ marginLeft: '15px', border: '1px solid #ccc', borderRadius: '5px', padding: '3px', width: '111px'}}
             />
           </div>
-
-          {!showModal && <button type="submit">Confirmar Reserva</button>}
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+          {!showModal && <button type="submit" style={{ backgroundColor: 'green', color: 'white', padding: '7px', border: 'none', borderRadius: '5px' }}>Confirmar Reserva</button>}
+          </div>
         </form>
       )}
 
-  {/* Modal de datos del usuario */}
-  {showModal && (
-        <div className="modal-background">
+      {/* Modal de datos del usuario */}
+      {showModal && (
+        <div className="modal-background" style={{  width: '350px', padding: '11px', border: '1px solid #ccc', borderRadius: '5px' }}>
           <div className="modal-content">
             <h2>Datos Usuario</h2>
             <p>Cédula: {cedula}</p>
@@ -241,7 +256,7 @@ const ModalDatosUsuario = ({ cedula, nombre, fechaIngreso, fechaSalida, tipoHabi
             <p>Fecha de Ingreso: {fechaIngreso}</p>
             <p>Fecha de Salida: {fechaSalida}</p>
             <p>Tipo de Habitación: {idHabitacion}</p>
-          
+
           </div>
         </div>
       )}
